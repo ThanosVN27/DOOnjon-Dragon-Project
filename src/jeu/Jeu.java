ea41e7d5c458def69ed317594b64f53c97114d7c
Le ThanhLong <thanh-long.le@etu.unistra.fr>
2025-06-02 10:44:46 +0200

    recuperer

--- src/jeu/Jeu.java
+++ src/jeu/Jeu.java
@@ -1,98 +1,91 @@
 package jeu;
 
-import personnage.Combattant;
-import personnage.MaitreDuJeu;
-import personnage.Monstre;
-import personnage.Joueur;
-
 import java.util.ArrayList;
 import java.util.List;
-import java.util.MissingFormatArgumentException;
+import personnage.Joueur;
+import personnage.MaitreDuJeu;
+import personnage.Monstre;
 
 
 public class Jeu {
     private final MaitreDuJeu maitreDuJeu;
-    private List<Combattant> listeJoueurs;
+    private List<Joueur> listeJoueurs;
+    private List<Monstre> listeMonstres;
+    private List<Object> listeEntite;
     private int tour;
 
     public Jeu() {
         this.maitreDuJeu = new MaitreDuJeu();
         this.tour = 0;
         listeJoueurs = new ArrayList<>();
+        listeMonstres = new ArrayList<>();
+        listeEntite = new ArrayList<>();
     }
 
     public void lancerJeu() {
         maitreDuJeu.miseEnPlace();
         afficherOrdreDeJeu();
-
-
     }
 
     public void jouerTour() {
         System.out.println("------------------Tour " + (tour + 1) + "-----------------");
+        tour++;
+        for (Object entite : listeEntite) {
+            if (entite instanceof Joueur) {
+                Joueur joueur = (Joueur) entite;
+                if (joueur.getPointsDeVie() > 0) {
+                    System.out.println(joueur.getNom() + " joue son tour.");
+                } else {
+                    System.out.println(joueur.getNom() + " est mort et ne peut pas jouer.");
+                }
+            } else if (entite instanceof Monstre) {
+                Monstre monstre = (Monstre) entite;
+                if (monstre.getPointsDeVie() > 0) {
+                    System.out.println(monstre.getNom() + " joue son tour.");
 
-
-
-
+                } else {
+                    System.out.println(monstre.getNom() + " est mort et ne peut pas jouer.");
+                }
+            }
+        }
 
     }
 
-
-
-
     public void afficherOrdreDeJeu() {
         System.out.println("---------------- Ordre de Jeu ----------------");
-        listeJoueurs = maitreDuJeu.getDonjon().ordreJeu();
-        for (Combattant c : listeJoueurs) {
-            if (c.estJoueur()) {
-                System.out.println("👤 Joueur  : " + c.afficherInfos() + " | Initiative : " + c.getInitiative());
-            } else {
-                System.out.println("👾 Monstre : " + c.afficherInfos() + " | Initiative : " + c.getInitiative());
+        listeJoueurs = maitreDuJeu.getDonjon().ordreJeuJoueur();
+        listeMonstres = maitreDuJeu.getDonjon().ordreJeuMonstre();
+        listeEntite.addAll(listeJoueurs);
+        listeEntite.addAll(listeMonstres);
+        listeEntite.sort((e1, e2) -> {
+            if (e1 instanceof Joueur && e2 instanceof Joueur)
+            {
+                return Integer.compare(((Joueur) e2).getInitiative(), ((Joueur) e1).getInitiative());
             }
-        }
+            else if (e1 instanceof Monstre && e2 instanceof Monstre)
+            {
+                return Integer.compare(((Monstre) e2).getInitiative(), ((Monstre) e1).getInitiative());
+            }
+            else if (e1 instanceof Joueur)
+            {
+                return -1;
+            }
+            else {
+                return 1;
+            }
+        });
 
         System.out.println("------------------------------------------------");
     }
 
     public void boucleJeu() {
+        boolean fin = false;
 
-    }
-
-
-
-
-
-    public void joueurTour() {
-        System.out.println("C'est au tour du joueur " + listeJoueurs.get(tour).getNom() + " de jouer.");
-
-        int optionRestant = 3;
-        for(Combattant c : listeJoueurs) {
-            if (c.estJoueur()) {
-                System.out.println("C'est au tour du joueur " + c.getNom() + " de jouer.");
-            }
-            while (optionRestant > 0) {
-                System.out.println("Options restantes : " + optionRestant);
-                System.out.println("1. Attaquer un monstre");
-                System.out.println("2. Se déplacer");
-                System.out.println("3. Passer le tour");
-                System.out.print("Choisissez une option (1-3) : ");
-                int choix;
-                System.out.println("Choisissez une option (1-3) : ");
-
-
+        while (!fin) {
+            jouerTour();
+            fin = true;
         }
 
-
-
-        }
-
-
-
-
-
+        System.out.println("🎉 Partie terminée !");
     }
-
-
-
-
 }
